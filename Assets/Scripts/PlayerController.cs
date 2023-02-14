8bc230901e45072d11b0efa1b0e3e1c71772d571
Daniel <1273602+dpaniagua0@users.noreply.github.com>
2023-02-14 00:09:36 -0600

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] float torqueAmount = 1f;
+    Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        RotatePlayer();
+    }
+
+    void RotatePlayer() {
+        if(Input.GetKey(KeyCode.LeftArrow)) {
+            Debug.Log("Left");
+            rb2d.AddTorque(torqueAmount);
+        } else if(Input.GetKey(KeyCode.RightArrow)) {
+            Debug.Log("Left");
+            rb2d.AddTorque(-torqueAmount);
+        }
+    }
+}
